47fd2ec1cce43992a85b02faa6dc6e0efefc4b4d
younusFoysal <younus.faysal@gmail.com>
2024-09-09 04:52:50 +0600

    done

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 8000
 
 // middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://stayvista-new.web.app'],
   credentials: true,
   optionSuccessStatus: 200,
 }
